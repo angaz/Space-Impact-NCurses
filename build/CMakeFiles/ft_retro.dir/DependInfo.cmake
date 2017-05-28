047d0fbea20a9ea97acab7413730e71fbc6c0d89
Angus Dippenaar <adippena@student.42.fr>
2017-05-28 16:59:08 +0200

    cmake

---
+++ build/CMakeFiles/ft_retro.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/nfs/zfs-student-6/users/adippena/Documents/cpp-bootcamp/rush00/src/Bullet/Bullet.cpp" "/nfs/zfs-student-6/users/adippena/Documents/cpp-bootcamp/rush00/build/CMakeFiles/ft_retro.dir/src/Bullet/Bullet.cpp.o"
+  "/nfs/zfs-student-6/users/adippena/Documents/cpp-bootcamp/rush00/src/Enemy/Enemy.cpp" "/nfs/zfs-student-6/users/adippena/Documents/cpp-bootcamp/rush00/build/CMakeFiles/ft_retro.dir/src/Enemy/Enemy.cpp.o"
+  "/nfs/zfs-student-6/users/adippena/Documents/cpp-bootcamp/rush00/src/Enemy/Zorg.cpp" "/nfs/zfs-student-6/users/adippena/Documents/cpp-bootcamp/rush00/build/CMakeFiles/ft_retro.dir/src/Enemy/Zorg.cpp.o"
+  "/nfs/zfs-student-6/users/adippena/Documents/cpp-bootcamp/rush00/src/LinkedList.cpp" "/nfs/zfs-student-6/users/adippena/Documents/cpp-bootcamp/rush00/build/CMakeFiles/ft_retro.dir/src/LinkedList.cpp.o"
+  "/nfs/zfs-student-6/users/adippena/Documents/cpp-bootcamp/rush00/src/Player/Player.cpp" "/nfs/zfs-student-6/users/adippena/Documents/cpp-bootcamp/rush00/build/CMakeFiles/ft_retro.dir/src/Player/Player.cpp.o"
+  "/nfs/zfs-student-6/users/adippena/Documents/cpp-bootcamp/rush00/src/Player/ShipOne.cpp" "/nfs/zfs-student-6/users/adippena/Documents/cpp-bootcamp/rush00/build/CMakeFiles/ft_retro.dir/src/Player/ShipOne.cpp.o"
+  "/nfs/zfs-student-6/users/adippena/Documents/cpp-bootcamp/rush00/src/actions.cpp" "/nfs/zfs-student-6/users/adippena/Documents/cpp-bootcamp/rush00/build/CMakeFiles/ft_retro.dir/src/actions.cpp.o"
+  "/nfs/zfs-student-6/users/adippena/Documents/cpp-bootcamp/rush00/src/gameLoop.cpp" "/nfs/zfs-student-6/users/adippena/Documents/cpp-bootcamp/rush00/build/CMakeFiles/ft_retro.dir/src/gameLoop.cpp.o"
+  "/nfs/zfs-student-6/users/adippena/Documents/cpp-bootcamp/rush00/src/main.cpp" "/nfs/zfs-student-6/users/adippena/Documents/cpp-bootcamp/rush00/build/CMakeFiles/ft_retro.dir/src/main.cpp.o"
+  "/nfs/zfs-student-6/users/adippena/Documents/cpp-bootcamp/rush00/src/obstacles.cpp" "/nfs/zfs-student-6/users/adippena/Documents/cpp-bootcamp/rush00/build/CMakeFiles/ft_retro.dir/src/obstacles.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../includes"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/ft_retro.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ft_retro.dir/src/main.cpp.o"
+  "CMakeFiles/ft_retro.dir/src/gameLoop.cpp.o"
+  "CMakeFiles/ft_retro.dir/src/actions.cpp.o"
+  "CMakeFiles/ft_retro.dir/src/obstacles.cpp.o"
+  "CMakeFiles/ft_retro.dir/src/LinkedList.cpp.o"
+  "CMakeFiles/ft_retro.dir/src/Bullet/Bullet.cpp.o"
+  "CMakeFiles/ft_retro.dir/src/Enemy/Enemy.cpp.o"
+  "CMakeFiles/ft_retro.dir/src/Enemy/Zorg.cpp.o"
+  "CMakeFiles/ft_retro.dir/src/Player/Player.cpp.o"
+  "CMakeFiles/ft_retro.dir/src/Player/ShipOne.cpp.o"
+  "ft_retro.pdb"
+  "ft_retro"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ft_retro.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
